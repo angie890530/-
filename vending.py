6fa63e7d71fe3b398dbf8d7ce53168c721aea753
user <b10721036@gmail.com>
2020-03-26 18:56:01 +0800

    what's change

---
+++ vending.py
@@ -0,0 +1,10 @@
+class Drink:
+    def __init__(self,name,price)
+        self.name=name
+        self.price=price
+    def get_name(self,new_name):
+        return self.name
+    def get_price(self,new_price):
+        return self.price 4re34wrfrtffdfdc
+
+
